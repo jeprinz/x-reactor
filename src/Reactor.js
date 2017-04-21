fe7846c7533f23502d415e39aed589d35b86eda1
jacob5555555555 <jacobeliasprinz@gmail.com>
2017-04-20 20:13:36 -0400

    Now works

--- src/Reactor.js
+++ src/Reactor.js
@@ -2,54 +2,74 @@
 
 import type {TwoWayMap} from './TwoWayMap';
 import {makeTwoWayMap} from './TwoWayMap';
+import {is} from 'immutable';
 
+type Variable = {
+  value: any,
+  func: () => any
+}
+
+const startValueMagic = Symbol();
 
 function makeHandler(): any{
-  const variables: Map<string, any> = new Map();
+  const variables: Map<string, Variable> = new Map();
   const keyDependsOnValue: TwoWayMap<string, string> = makeTwoWayMap();
   var referenced: Set<string> = new Set();
   var runningMagic = false;
 
-  //check who this variable depends on, and add it to those varible's dependsonme lists
-  function initialSetup(prop: string, func: () => any): void{
-    if (variables.has(prop)){
-      keyDependsOnValue.deleteValue(prop);
-    }
+  function setup(prop: string, func: () => any): void{
+    keyDependsOnValue.deleteKey(prop);
 
     runningMagic = true;
     referenced = new Set();
-    variables.set(prop, func());
-    for (const dependency of referenced){
-      keyDependsOnValue.set() // the problem is that it needs to be a multimap
+    variables.set(prop, {func, value: func()});
+    for (const dependency of referenced){//TODO: make a setAll method in TwoWayMap
+      keyDependsOnValue.set(prop, dependency);
     }
     runningMagic = false;
+
+    redo(prop, true);
   }
 
-  function subsequentSet(prop: string, func: () => any): void{
+  function redo(prop: string, alwaysDo: bool): void{
+    const variable = variables.get(prop);
+    if (variable){
+      const old = variable.value;
+      variable.value = variable.func();
 
+      if (!is(old, variable.value) || alwaysDo){
+        const dependsOnMe = keyDependsOnValue.getFromValue(prop);
+        for (const dependency of dependsOnMe){//update everybody that depended on me
+          redo(dependency, false);
+        }
+      }
+
+    } else {
+      throw Error("Something went wrong, variable was undefined");
+    }
   }
 
   const handler = {
     get: function(target, name){
       if (runningMagic){
         referenced.add(name);
-        return variables.get(name);
-      } else { //should I throw?
-        return variables.get(name);
+      }
+      const variable = variables.get(name);
+      if (variable){
+        return variable.value;
+      } else {
+        throw Error("invalid variable name " + name);
       }
     },
     set: function(obj, prop, val){
       if (typeof prop !== "string"){
         throw Error("Reactor variables should be referenced with a string or Symbol");
       }
-      if (typeof prop !== "function"){
+      if (typeof val !== "function"){
         throw Error("Reactor variables should be set to functions");
       }
-      if (variables.has(prop)){
-
-      } else {
-        initialSetup(prop, val);
-      }
+      setup(prop, val);
+      return true;
     }
   }
   return handler;
--- src/test.js
+++ src/test.js
@@ -1,1 +1,16 @@
-// @flow
+
+import {makeReactor} from './Reactor';
+
+const VAR = makeReactor();
+
+VAR.a = () => 1;
+
+VAR.b = () => 2;
+
+VAR.c = () => VAR.a + VAR.b;
+
+console.log(VAR.c)
+
+VAR.a = () => 5;
+
+console.log(VAR.c)
