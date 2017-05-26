b816fb217f51bc9bb139644670fc9f92bf83fbc1
jacob5555555555 <jacobeliasprinz@gmail.com>
2017-05-26 02:32:48 -0400

    More tests and README
    Tests are now pretty extensive, and the README has an explanation

--- test/basic-functionality.js
+++ test/basic-functionality.js
@@ -23,4 +23,70 @@ describe('get function', function(){
     a.set(() => 3);
     assert(a.get() == 3);
   });
+
+  it('throws when xget is called outside of constructor or setter', () => {
+    const a = xvar(() => 0);
+    try {
+      a.xget();
+      assert(false);
+    } catch (e){
+    }
+  })
+
+  it('changes value of one xvar that depends on another', () => {
+    const a = xvar(() => 5);
+    const b = xvar(() => a.xget() + 1);
+    assert(b.get() == 6);
+
+    a.set(() => 0);
+    assert(b.get() == 1);
+  });
+
+  it('changes value of depending xvar when set is used', () => {
+    const a = xvar(() => 0);
+    const b = xvar(() => 1);
+
+    b.set(() => a.xget());
+    assert(b.get() == 0);
+
+    a.set(() => 2);
+    assert(b.get() == 2);
+  })
+
+  it('called onUpdate registered functions when appropritate', () => {
+    const a = xvar(() => 0);
+    var called = 0;
+    a.onUpdate(() => {called += 1});
+    assert(called == 0);
+    a.set(() => 1);
+    assert(called = 1);
+    a.set(() => 1);
+    assert(called = 1);
+  });
+
+  it('deals with dependencies changing', () => {
+    const a = xvar(() => 1);
+    const b = xvar(() => 2);
+    const c = xvar(() => true);
+
+    const result: XVar<number> = xvar(() => {
+      if (c.xget()) return a.xget(); else return b.xget();
+    });
+
+    assert(result.get() == 1);
+    c.set(() => false);
+    assert(result.get() == 2);
+
+    //now add another xvar
+    const d = xvar(() => 8);
+    result.set(() => {
+      if (c.xget()) return d.xget(); else return b.xget();
+    });
+
+    assert(result.get() == 2);
+    c.set(() => true);
+    assert(result.get() == 8);
+    d.set(() => 7);
+    assert(result.get() == 7);
+  });
 })
